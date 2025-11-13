520ad8f18204d8aaf4943136bb43feddcd97f1d3
AmrrElShazlyy <amrr.m.elshazly@gmail.com>
2025-11-13 18:14:49 +0200

    Project setup: Add initial Xcode project for CountryLookup

---
+++ CountryLookup/CountryLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryLookupApp.swift
+//  CountryLookup
+//
+//  Created by Amr El Shazly on 13/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
