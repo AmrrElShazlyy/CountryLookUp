1b70241cf0c1c3b6f91f691e717827d127d4ddd6
AmrrElShazlyy <amrr.m.elshazly@gmail.com>
2025-11-13 23:36:28 +0200

    test: add unit tests for the network layer
    Add tests to ensure making correct requests, parsing data, and handling responses as expected

---
+++ CountryLookupTests/NetworkingTests/Mocks/MockCountryServiceProvider.swift
@@ -0,0 +1,23 @@
+//
+//  MockCountryServiceProvider.swift
+//  CountryLookupTests
+//
+//  Created by Amr El Shazly on 13/11/2025.
+//
+
+import Foundation
+import Combine
+@testable import CountryLookup
+
+@MainActor
+class MockCountryServiceProvider: CountryService {
+    var apiClient: MockAPIClient<CountryEndpoint>
+    
+    init(apiClient: MockAPIClient<CountryEndpoint>) {
+        self.apiClient = apiClient
+    }
+    
+    func fetchCountry(name: String) -> AnyPublisher<[Country], CountryLookup.APIError> {
+        return apiClient.request(.country(name: name))
+    }
+}
